8926980ed2fd4ea2490f4875f888faae76ebb4db
24amishra <144934356+24amishra@users.noreply.github.com>
2025-07-30 17:56:20 -0400

    add

--- front-end/src/app/auth/route.js
+++ front-end/src/app/auth/route.js
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+
+  const API_BASE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://lifestyle-api.onrender.com/'
+  : 'http://localhost:5000';
   return NextResponse.redirect(`${API_BASE_URL}/authorize`);
 }
