bf77094ff45fa75e8ff5b95ab4f4278c96029f0c
24amishra <144934356+24amishra@users.noreply.github.com>
2025-07-29 23:01:12 -0400

    little bug

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Remove this line: output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  // Or specifically disable this rule
-  eslint: {
-    rules: {
-      'react/no-unescaped-entities': 'off'
-    }
   }
 }
 
