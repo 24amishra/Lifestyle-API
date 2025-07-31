8dd77b944744f2470c6256661a581ce83a92276e
24amishra <144934356+24amishra@users.noreply.github.com>
2025-07-31 19:16:28 -0400

    fixed the url

--- front-end/src/app/auth/route.js
+++ front-end/src/app/auth/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
 
   const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://lifestyle-api.onrender.com/'
+  ? 'https://lifestyle-api.onrender.com'
   : 'http://localhost:5000';
   return NextResponse.redirect(`${API_BASE_URL}/authorize`);
 }
